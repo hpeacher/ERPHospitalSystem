a9132d2e0ed990bfa7ec612b818017c0d8ce6937
Aidan <asousley@iastate.edu>
2025-11-19 21:45:24 -0600

    Little changes

--- ERPHospitalSystem/src/com/hospital/command/impl/ViewPatientRecordCommand.java
+++ ERPHospitalSystem/src/com/hospital/command/impl/ViewPatientRecordCommand.java
@@ -1,6 +1,5 @@
 package com.hospital.command.impl;
 
-import java.util.ArrayList;
 import java.util.Scanner;
 import com.hospital.command.ICommand;
 import com.hospital.model.PatientAdministrativeInfo;
@@ -27,26 +26,12 @@ public class ViewPatientRecordCommand implements ICommand{
 
     @Override
     public void execute() {
-        PatientAdministrativeInfo ai = new PatientAdministrativeInfo("p5", "John Doe", "1990-01-01", "123-456-7890", "123 Main St");
-       PatientMedicalInfo mi = new PatientMedicalInfo("p5", "Male", "O+", "180 cm", "75 kg");
-       PatientRecord record = new PatientRecord("p5", ai, mi, "Health Insurance Co.");
-
-
-       ArrayList<VisitRecord> visits = new ArrayList<>();
-       visits.add(new VisitRecord("p5", "2024-01-01"));
-       visits.add(new VisitRecord("p5", "2024-02-01"));
-       visits.add(new VisitRecord("p5", "2024-03-01"));
-       visits.add(new VisitRecord("p5", "2024-04-01"));
-       visits.add(new VisitRecord("p5", "2024-05-01"));
-       visits.add(new VisitRecord("p5", "2024-06-01"));
-       visits.add(new VisitRecord("p5", "2024-07-01"));
-       record.setVisits(visits);
         System.out.println("=== View Patient Record ===");
 
         System.out.println("Enter Patient Id: ");
         String patientId = sc.nextLine();
 
-        //PatientRecord record = fileManager.getPatientRecord(patientId);
+        PatientRecord record = fileManager.getPatientRecord(patientId);
 
         if (record == null) {
             System.out.println("Patient record not found.");
